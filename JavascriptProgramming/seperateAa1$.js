56ce767d2f7d7eec43958beb07f8377739f8bf6d
ajax <anuprajjathanna@gmail.com>
2023-03-01 17:22:05 +0530

    commit 7

--- JavascriptProgramming/seperateAa1$.js
+++ JavascriptProgramming/seperateAa1$.js
@@ -1,11 +1,12 @@
-var st='AxB1#*eZ8'
+var st='AxB1#*45eZ820'
 var arr=st.split('')
 
 var uppercase=''
 var lowercase=''
 var number=''
 var specialcharacter=''
-for (const key in arr) 
+var x=0;
+for (let key = 0; key < arr.length; key++) 
 {
     if (arr[key]>='A'&&arr[key]<='Z') 
     {
@@ -17,14 +18,35 @@ for (const key in arr)
     }
     else if(arr[key]>='0'&&arr[key]<='9')
     {
-        number=number+arr[key]
+        if(x==key)
+        {
+            number=number+arr[key]
+        }
+        else
+        {
+            number=number+' '+arr[key]
+        }
+        x=key+1
+
     }
     else
     {
         specialcharacter=specialcharacter+arr[key]
     }
 }
+
+
 console.log(`uppercase--->${uppercase}`);
 console.log(`lowercase--->${lowercase}`);
-console.log(`number--->${number}`);
+console.log(`number--->${number.trim()}`);
 console.log(`specialcharacter--->${specialcharacter}`);
+
+let num=number.trim().split(' ')
+let sum=0
+for (let i = 0; i < num.length; i++) 
+{
+    n=Number(num[i])
+    sum=sum+n
+}
+
+console.log('sum---->'+sum);
